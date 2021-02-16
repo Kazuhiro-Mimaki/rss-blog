2d230e967161fd8048f41db4347a3dd10218b000
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2021-02-16 14:35:14 +0900

    feat: delete member section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,11 @@
 import { NextPage } from "next";
-import Link from "next/link";
 
 import posts from "@.contents/posts.json";
 import { config } from "@site.config";
 import { PostItem } from "@src/types";
-import { ScrollableMembers } from "@src/components/ScrollableMembers";
 import { PostList } from "@src/components/PostList";
 import { PageSEO } from "@src/components/PageSEO";
-import {
-  ContentWrapper,
-  UndoWrapForScroll,
-} from "@src/components/ContentWrapper";
+import { ContentWrapper } from "@src/components/ContentWrapper";
 
 const Page: NextPage = () => {
   return (
@@ -30,23 +25,6 @@ const Page: NextPage = () => {
               {config.siteMeta.description}
             </p>
           )}
-        </ContentWrapper>
-      </section>
-
-      <section className="home-members">
-        <ContentWrapper>
-          <div className="home-section-title-container">
-            <h2 className="home-section-title">Members</h2>
-            <Link href="/members">
-              <a className="home-section-link">See Details →</a>
-            </Link>
-          </div>
-
-          <div className="home-members-container">
-            <UndoWrapForScroll>
-              <ScrollableMembers />
-            </UndoWrapForScroll>
-          </div>
         </ContentWrapper>
       </section>
 
